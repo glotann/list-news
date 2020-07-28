0eabcc414da52b7eb35b7bab44b74ee42f5f04f9
Glory Tannia <46555657+glotann@users.noreply.github.com>
2020-07-28 13:24:51 +0700

    first push

---
+++ android/app/src/main/kotlin/com/example/list_view_kompas/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.list_view_kompas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Testing list view',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.orange,),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('List View Kompas'),
+      ),
+      body: Container(),
+    );
+  }
+}
+
