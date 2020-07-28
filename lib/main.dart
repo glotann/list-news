d1a81091119079a381b3270feff5cd4d1fb54a50
wahyukharisma <madewahyu39@gmail.com>
2020-07-28 20:56:34 +0700

    Using future builder for infinite listview
    ! Untuk metode ini bisa dilakukan dan mendapatkan kode yang bersih dan compile yang bebas bug tetapi kekurangannya setiap melakukan pengambilan data melalui service ui otomatis rebuild karena memanggil setState
    ? Solusi sementara tetap menggunakan state management (ex BLoC)

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:list_view_kompas/models/news_model.dart';
 import 'package:list_view_kompas/services/news_service.dart';
-
 
 void main() => runApp(MyApp());
 
@@ -26,17 +26,20 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String text = 'Nama';
+  String cursor;
   ScrollController controller = ScrollController();
+  List<News> news = [];
 
-  void onScroll(){
+  void onScroll() {
     double maxScroll = controller.position.maxScrollExtent;
     double currentScroll = controller.position.pixels;
 
-    if(currentScroll == maxScroll){
+    if (currentScroll == maxScroll) {
       //TODO : Implemetasi mengambil data dari api service
+      setState(() {});
     }
   }
+
   @override
   Widget build(BuildContext context) {
     Size mediaQuery = MediaQuery.of(context).size;
@@ -48,101 +51,29 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Center(
         child: FutureBuilder<NewsListResult>(
-          future: NewsListResult.funcGetListNews(),
+          future: NewsListResult.funcGetListNews(cursor),
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.done) {
               if (snapshot.hasError) {
                 print(snapshot.error.toString());
-                return Text("Tidak ada data");
+                return Text("Error");
               } else {
+                news.addAll(snapshot.data.listNews);
+                cursor = snapshot.data.cursor;
                 return ListView.builder(
                   controller: controller,
-                  itemCount: snapshot.data.listNews.length,
-                  itemBuilder: (ctx, i) {
-                    return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        CachedNetworkImage(
-                          height: mediaQuery.height * 0.25,
-                          width: double.infinity,
-                          fit: BoxFit.cover,
-                          imageUrl: snapshot.data.listNews[i].imageUrl,
-                          progressIndicatorBuilder: (ctx, url, downloadProgress) =>
-                          new CircularProgressIndicator(
-                            value: downloadProgress.progress,
-                          ),
-                          errorWidget: (ctx, url, error) => new Icon(Icons.error),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 12.0, top: 12.0, right: 12.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              HtmlWidget(
-                                snapshot.data.listNews[i].title,
-                                bodyPadding: EdgeInsets.all(0),
-                                webView: true,
-                                textStyle: GoogleFonts.merriweather(
-                                    fontSize: 24, fontWeight: FontWeight.bold),
-                              ),
-                              SizedBox(
-                                height: 12,
-                              ),
-                              Text(
-                                snapshot.data.listNews[i].description,
-                                style: GoogleFonts.openSans(fontSize: 16, height: 1.5),
-                                overflow: TextOverflow.ellipsis,
-                                maxLines: 3,
-                              ),
-                              SizedBox(
-                                height: 12,
-                              ),
-                              Row(
-                                children: <Widget>[
-                                  Text(
-                                    "-",
-                                    style: GoogleFonts.roboto(color: Colors.grey),
-                                  ),
-                                  Container(
-                                    width: mediaQuery.width * 0.5,
-                                    alignment: Alignment.centerLeft,
-                                    child: FittedBox(
-                                      child: FlatButton(
-                                        child: HtmlWidget(
-                                          "-",
-                                          webView: true,
-                                          bodyPadding: EdgeInsets.all(0),
-                                          textStyle: GoogleFonts.roboto(
-                                            color: Theme.of(context).primaryColor,
-                                          ),
-                                        ),
-                                        // onPressed: () => _selectNews(context),
-                                        onPressed: () {
-                                          //TODO : Implemented function show by category
-                                        },
-                                      ),
-                                    ),
-                                  ),
-                                  Expanded(
-                                    child: Align(
-                                      alignment: Alignment.topRight,
-                                      child: GestureDetector(
-                                        onTap: () {
-                                          final RenderBox box = context.findRenderObject();
-
-                                        },
-                                        child: Icon(Icons.share),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
+                  itemCount: news.length + 1,
+                  itemBuilder: (ctx, i) => (i < news.length)
+                      ? buildItem(mediaQuery, news, i, context)
+                      : Container(
+                          child: Center(
+                            child: SizedBox(
+                              width: 30,
+                              height: 30,
+                              child: CircularProgressIndicator(),
+                            ),
                           ),
                         ),
-                      ],
-                    );
-                  },
                 );
               }
             } else {
@@ -153,6 +84,90 @@ class _HomePageState extends State<HomePage> {
           },
         ),
       ),
+    );
+  }
+
+  Column buildItem(
+      Size mediaQuery, List<News> snapshot, int i, BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        CachedNetworkImage(
+          height: mediaQuery.height * 0.25,
+          width: double.infinity,
+          fit: BoxFit.cover,
+          imageUrl: snapshot[i].imageUrl,
+          progressIndicatorBuilder: (ctx, url, downloadProgress) =>
+              new CircularProgressIndicator(
+            value: downloadProgress.progress,
+          ),
+          errorWidget: (ctx, url, error) => new Icon(Icons.error),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 12.0, top: 12.0, right: 12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              HtmlWidget(
+                snapshot[i].title,
+                bodyPadding: EdgeInsets.all(0),
+                webView: true,
+                textStyle: GoogleFonts.merriweather(
+                    fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Text(
+                snapshot[i].description,
+                style: GoogleFonts.openSans(fontSize: 16, height: 1.5),
+                overflow: TextOverflow.ellipsis,
+                maxLines: 3,
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Row(
+                children: <Widget>[
+                  Text(
+                    "-",
+                    style: GoogleFonts.roboto(color: Colors.grey),
+                  ),
+                  Container(
+                    width: mediaQuery.width * 0.5,
+                    alignment: Alignment.centerLeft,
+                    child: FittedBox(
+                      child: FlatButton(
+                        child: HtmlWidget(
+                          "-",
+                          webView: true,
+                          bodyPadding: EdgeInsets.all(0),
+                          textStyle: GoogleFonts.roboto(
+                            color: Theme.of(context).primaryColor,
+                          ),
+                        ),
+                        // onPressed: () => _selectNews(context),
+                        onPressed: () {
+                          //TODO : Implemented function show by category
+                        },
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.topRight,
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: Icon(Icons.share),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -5,20 +5,33 @@ import 'package:http/http.dart' as http;
 
 import '../models/news_model.dart';
 
-class NewsListResult{
+class NewsListResult {
   List<News> listNews;
+  String cursor;
 
-  NewsListResult({
-    this.listNews
-  });
+  NewsListResult({this.listNews, this.cursor});
 
   factory NewsListResult.getListResult(NewsListResult data) {
-    return NewsListResult(listNews: data.listNews);
+    return NewsListResult(
+      listNews: data.listNews,
+      cursor: data.cursor,
+    );
   }
 
-  static Future<NewsListResult> funcGetListNews() async{
-    String url = "https://apikid.kompas.id/v2/article/list/terms?&siteid=1&timestamp=2020-07-28 00:00:00";
-    String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9hcGlnZW4ua29tcGFzLmlkIiwiaWF0IjoxNTk1OTMzMDE5LCJleHAiOjE1OTU5MzM5MTksImRhdGEiOnsiaWQiOiI0YzdiOWNmMC0zYTg5LTQwNGYtOGZmOC0xM2FlZGMxN2NlOGQiLCJlbWFpbCI6ImFub255bm91cy51c2VyQGtvbXBhcy5pZCIsInJvbGUiOlsic3Vic2NyaWJlciJdfX0.fBvRFCyTLUZR3h7S7IOsa2hI5RIJexucoN0WK5STKpexkpOGk5k7ns3SQZLbWsK2C99QrGYURV8E7NgMqoqepclzG3YVTtynD3wSRU_QO-Uhlh4YUXKaGHO1hJaur082k-unh0SOvKZtb-hC3G4W9OTV_-ZltLjBpYEJwTNJS_U";
+  static Future<NewsListResult> funcGetListNews(String cursor) async {
+    String url;
+    if (cursor == null) {
+      url =
+          "https://apikid.kompas.id/v2/article/list/terms?&siteid=1&timestamp=2020-07-28 00:00:00";
+    } else {
+      url =
+          "https://apikid.kompas.id/v2/article/list/terms?&siteid=1&timestamp=2020-07-28 00:00:00&cursor=" +
+              cursor;
+    }
+
+    String token =
+        "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9hcGlnZW4ua29tcGFzLmlkIiwiaWF0IjoxNTk1OTQzNzk2LCJleHAiOjE1OTU5NDQ2OTYsImRhdGEiOnsiaWQiOiI5YmQyNTZhZi0yNzI3LTQzMWMtYTZkZS01NDg4M2YxNmNjMjgiLCJlbWFpbCI6Im1hZGUud2FoeXVAa29tcGFzLmNvbSIsInJvbGUiOlsic3Vic2NyaWJlciJdfX0.rF8f4JjfSOjfgu2rYwHNiATLNOFGuhJL0clfoj5rYS04lHT0rwBan4qdxvj88AuhyYl9qEFJkWKdr9aC1Cygqv-gnQZ8fVCObW-yJNe3jjxl-coapg4GS9MlIScCDBV-nWbAuFkTUBiMxxyFHGG4Age7IBq5BQ_fUr8N8tVfMrE";
+
     List<News> news = [];
 
     final response = await http.get(url,
@@ -29,7 +42,7 @@ class NewsListResult{
 
     List<News> temp = [];
 
-    try{
+    try {
       for (var i in responseJson['result']['articles']) {
         News getListNews = News(
           postName: i['name'],
@@ -38,16 +51,18 @@ class NewsListResult{
           imageUrl: i['thumbnails']['sizes']['thumbnail_medium']['permalink'],
         );
         temp.add(getListNews);
+        cursor = responseJson['result']['meta']['next'];
       }
-    }
-    catch(e){
+    } catch (e) {
       print('Error occurs: $e');
     }
     news.addAll(temp);
 
-    NewsListResult contentResult = new NewsListResult(listNews: news);
+    NewsListResult contentResult = new NewsListResult(
+      listNews: news,
+      cursor: cursor,
+    );
 
     return NewsListResult.getListResult(contentResult);
   }
-
 }
